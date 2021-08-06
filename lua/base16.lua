11cc48153621a0411584277c9567964601f28e21
siduck76 <siduckk76@protonmail.com>
2021-08-06 21:20:15 +0530

    better color blue color for tokyonight theme

--- lua/base16.lua
+++ lua/base16.lua
@@ -772,7 +772,7 @@ themes["tokyonight"] = {
         base00 = "1A1B26"; base01 = "3b4261"; base02 = "3b4261"; base03 = "545c7e";
         base04 = "565c64"; base05 = "a9b1d6"; base06 = "bbc5f0"; base07 = "c0caf5";
         base08 = "f7768e"; base09 = "ff9e64"; base0A = "e0af68"; base0B = "9ece6a";
-        base0C = "98c0ff"; base0D = "7aa2f7"; base0E = "bb9af7"; base0F = "db4b4b";
+        base0C = "2ac3de"; base0D = "7aa2f7"; base0E = "bb9af7"; base0F = "db4b4b";
 }
 themes["harmonic-dark"] = {
 	base00 = "0b1c2c"; base01 = "223b54"; base02 = "405c79"; base03 = "627e99";
