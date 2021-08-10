91dac294cebd9f4fc9ee162e64d30d0aa4e18d53
siduck76 <siduckk76@protonmail.com>
2021-08-10 20:08:49 +0530

    add chadracula theme

---
+++ lua/themes/chadracula-base16.lua
@@ -0,0 +1,18 @@
+return {
+    base0A = "FF92D0",
+    base04 = "555763",
+    base07 = "f7f7fb",
+    base05 = "ededf1",
+    base0E = "c298fe",
+    base0D = "8BE9FD",
+    base0C = "FF5555",
+    base0B = "F4F99D",
+    base02 = "3d3f4b",
+    base0F = "FF6E67",
+    base03 = "50525e",
+    base08 = "FF5555",
+    base01 = "323440",
+    base00 = "282A36",
+    base09 = "56ff81",
+    base06 = "f1f2f8"
+}
