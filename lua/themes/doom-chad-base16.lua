3efab6817be364a9ee004f6afc91fdcd4ee72b2e
siduck76 <siduckk76@protonmail.com>
2021-10-05 13:29:06 +0530

    added doom-chad theme

---
+++ lua/themes/doom-chad-base16.lua
@@ -0,0 +1,18 @@
+return {
+   base00 = "282c34",
+   base01 = "32363e",
+   base02 = "3c4048",
+   base03 = "4e525a",
+   base04 = "5a5e66",
+   base05 = "a7aebb",
+   base06 = "b3bac7",
+   base07 = "bbc2cf",
+   base08 = "ff6c6b",
+   base09 = "da8548",
+   base0A = "ECBE7B",
+   base0B = "98be65",
+   base0C = "66c4ff",
+   base0D = "48a6e6",
+   base0E = "9c94d4",
+   base0F = "c85a50",
+}
