42cb2e8e42b188506ba3f96b116e63ec883ecb4a
Tim Culverhouse <tim@timculverhouse.com>
2021-10-21 09:34:13 -0500

    Add onedark-deep theme

---
+++ lua/hl_themes/onedark-deep.lua
@@ -0,0 +1,35 @@
+local colors = {
+   white = "#6c7d9c",
+   darker_black = "#141b28",
+   black = "#1a212e", --  nvim bg
+   black2 = "#202734",
+   one_bg = "#242b38", -- real bg of onedark
+   one_bg2 = "#2d3441",
+   one_bg3 = "#353c49",
+   grey = "#455574",
+   grey_fg = "#4f5f7e",
+   grey_fg2 = "#596988",
+   light_grey = "#617190",
+   red = "#f65866",
+   baby_pink = "#e06c75",
+   pink = "#ff75a0",
+   line = "#29303d", -- for lines like vertsplit
+   green = "#8bcd5b",
+   vibrant_green = "#98c379",
+   nord_blue = "#52a0e0",
+   blue = "#41a7fc",
+   yellow = "#ebc275",
+   sun = "#e5c07b",
+   purple = "#c678dd",
+   dark_purple = "#c678dd",
+   teal = "#34bfd0",
+   orange = "#ea8912",
+   cyan = "#56b6c2",
+   statusline_bg = "#1e2532",
+   lightbg = "#2f333b",
+   lightbg2 = "#292d35",
+   pmenu_bg = "#98c379",
+   folder_bg = "#41a7fc",
+}
+
+return colors
---
+++ lua/themes/onedark-deep-base16.lua
@@ -0,0 +1,18 @@
+return {
+   base00 = "1a212e",
+   base01 = "1e2532",
+   base02 = "21283b",
+   base03 = "4f5f7e",
+   base04 = "596988",
+   base05 = "6c7d9c",
+   base06 = "b6bdca",
+   base07 = "c8ccd4",
+   base08 = "f65866",
+   base09 = "ea8912",
+   base0A = "ebc275",
+   base0B = "8bcd5b",
+   base0C = "52a0e0",
+   base0D = "41a7fc",
+   base0E = "c678dd",
+   base0F = "f65866",
+}
