32dcf853d5744aa7859e510da15447689046e0c5
manas140 <manasbhosale.14@gmail.com>
2021-11-27 10:08:46 +0000

    added base16-classic-dark theme

---
+++ lua/themes/classic-dark-base16.lua
@@ -0,0 +1,18 @@
+return {
+   base00 = "151515",
+   base01 = "1f1f1f",
+   base02 = "282828",
+   base03 = "1b1b1b",
+   base04 = "d0d0d0",
+   base05 = "d0d0d0",
+   base06 = "e0e0e0",
+   base07 = "1f1f1f",
+   base08 = "AC4142",
+   base09 = "F4BF75",
+   base0A = "F4BF75",
+   base0B = "90A959",
+   base0C = "75B5AA",
+   base0D = "6A9FB5",
+   base0E = "AA759F",
+   base0F = "F4BF75",
+}
