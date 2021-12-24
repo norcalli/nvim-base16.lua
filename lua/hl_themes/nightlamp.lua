f75f9e570fc1c1c0d88e15f263af0cf71bcc1cd5
boppyt <71049646+boppyt@users.noreply.github.com>
2021-12-25 08:41:27 +0900

    add nightlamp theme

---
+++ lua/hl_themes/nightlamp.lua
@@ -0,0 +1,36 @@
+local colors = {
+    white = "#e0d6bd",
+    darker_black = "#16171c",
+    black = "#18191f", --  nvim bg
+    black2 = "#2c2e38",
+    one_bg = "#404452",
+    one_bg2 = "#55596b",
+    one_bg3 = "#686d85",
+    grey = "#5f6378",
+    grey_fg = "#6c7189",
+    grey_fg2 = "#7b7f97",
+    light_grey = "#8b8fa4",
+    red = "#a67476",
+    baby_pink = "#f7eaed",
+    pink = "#d1a3b1",
+    line = "#2d2f2f", -- for lines like vertsplit
+    green = "#8c9383",
+    vibrant_green = "#909963",
+    nord_blue = "#8d9bb3",
+    blue = "#5a6986",
+    yellow = "#ccb89c",
+    sun = "#deb88a",
+    purple = "#958793",
+    dark_purple = "#cbc1e3",
+    teal = "#7fb1af",
+    orange = "#c8916d",
+    cyan = "#90a0a0",
+    statusline_bg = "#22242c",
+    lightbg = "#282c3c",
+    lightbg2 = "#2c2e38",
+    pmenu_bg = "#90a0a0",
+    folder_bg = "#90a0a0",
+ }
+ 
+ return colors
+ 
---
+++ lua/themes/nightlamp-base16.lua
@@ -0,0 +1,19 @@
+return {
+    base00 = "18191f",
+    base01 = "3b3837",
+    base02 = "4e4a47",
+    base03 = "625c58",
+    base04 = "b5ac9b",
+    base05 = "cbc0ab",
+    base06 = "e0d6bd",
+    base07 = "f1ebd1",
+    base08 = "cbc1e3",
+    base09 = "a67476",
+    base0A = "deb88a",
+    base0B = "8c9383",
+    base0C = "90a0a0",
+    base0D = "c495a2",
+    base0E = "8d9bb3",
+    base0F = "d0d0c7",
+ }
+ 
