b00f582516bedce1b58fc62062d8d45ac59dfe19
boppyt <71049646+boppyt@users.noreply.github.com>
2021-12-25 19:16:19 +0900

    nightlamp: more pleasing look

--- lua/themes/nightlamp-base16.lua
+++ lua/themes/nightlamp-base16.lua
@@ -7,12 +7,13 @@ return {
     base05 = "cbc0ab",
     base06 = "e0d6bd",
     base07 = "f1ebd1",
-    base08 = "b17f81",
-    base09 = "c8916d",
-    base0A = "deb88a",
+    base08 = "b8aad9",
+    base09 = "b17f81",
+    base0A = "d4ae80",
     base0B = "8aa387",
-    base0C = "96a6a6",
+    base0C = "9ba292",
     base0D = "c495a2",
     base0E = "8896ae",
     base0F = "d0d0c7",
  }
+ 
