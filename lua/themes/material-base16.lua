64276b55e5be654078be1d71d6e1a19e10bfaf56
Phan Long <long2002tb@gmail.com>
2022-03-07 23:27:24 +0700

    add material theme

---
+++ lua/themes/material-base16.lua
@@ -0,0 +1,18 @@
+return {
+    base00 = "1e222a",
+    base01 = "353b45",
+    base02 = "3e4451",
+    base03 = "4a4a4a",
+    base04 = "b2ccd6",
+    base05 = "eeffff",
+    base06 = "eeffff",
+    base07 = "ffffff",
+    base08 = "f07178",
+    base09 = "f78c6c",
+    base0A = "ffcb6b",
+    base0B = "c3e88d",
+    base0C = "89ddff",
+    base0D = "82aaff",
+    base0E = "c792ea",
+    base0F = "ff5370"
+}
