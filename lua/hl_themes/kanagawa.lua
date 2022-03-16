a7bb1adf7297c6c00c8f8513705bda2f28fa54fa
manjirou <maxim.mametkulov@halbestunde.com>
2022-03-16 22:03:28 +0600

    Kanagawa theme added

---
+++ lua/hl_themes/kanagawa.lua
@@ -0,0 +1,35 @@
+local colors = {
+    white = "#DCD7BA",
+    darker_black = "#16161D",
+    black = "#1F1F28", --  nvim bg
+    black2 = "#2A2A37",
+    one_bg = "#1F1F28",
+    one_bg2 = "#2A2A37",
+    one_bg3 = "#363646",
+    grey = "#54546D",
+    grey_fg = "#54546D",
+    grey_fg2 = "#56566f",
+    light_grey = "#5e5e77",
+    red = "#FF5D62",
+    baby_pink = "#D27E99",
+    pink = "#c8748f",
+    line = "#252535", -- for lines like vertsplit
+    green = "#76946A",
+    vibrant_green = "#98BB6C",
+    nord_blue = "#2D4F67",
+    blue = "#658594",
+    yellow = "#FF9E3B",
+    sun = "#FFA066",
+    purple = "#957FB8",
+    dark_purple = "#8b75ae",
+    teal = "#7AA89F",
+    orange = "#fa9b61",
+    cyan = "#A3D4D5",
+    statusline_bg = "#C8C093",
+    lightbg = "#2A2A37",
+    lightbg2 = "#363646",
+    pmenu_bg = "#223249",
+    folder_bg = "#354A77",
+}
+
+return colors
