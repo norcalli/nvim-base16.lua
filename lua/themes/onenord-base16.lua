2ae23be472bdf32889e63341f74cfaf8ba1c4847
siduck <siduck@tutanota.com>
2022-04-03 11:30:15 +0530

    improve onenord theme

--- lua/themes/onenord-base16.lua
+++ lua/themes/onenord-base16.lua
@@ -8,11 +8,11 @@ return {
     base0C = "97b7d7",
     base0B = "A3BE8C",
     base02 = "434C5E",
-    base0F = "BF616A",
+    base0F = "d57780",
     base03 = "4C566A",
-    base08 = "ba5c65",
+    base08 = "d57780",
     base01 = "3B4252",
-    base00 = "2E3440",
-    base09 = "D08770",
+    base00 = "2a303c",
+    base09 = "e39a83",
     base06 = "c7cdd8"
 }
