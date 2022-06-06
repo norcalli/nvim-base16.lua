d0f2ec5b8d7c4b076afa294b1611dd918d210c26
siduck <siduck@tutanota.com>
2022-06-06 10:01:16 +0530

    Update gatekeeper.lua

--- lua/hl_themes/gatekeeper.lua
+++ lua/hl_themes/gatekeeper.lua
@@ -1,57 +1,63 @@
 local M = {}
 
 M.base_30 = {
-   white = "#ab5236",
-   darker_black = "#000000",
+   white = "#cccdd1",
+   darker_black = "#0a0a0a",
    black = "#101010", --  nvim bg
-   black2 = "#161616",
-   one_bg = "#1a1a1a",
-   one_bg2 = "#232323",
-   one_bg3 = "#2b2b2b",
-   grey = "#ff5236",
-   grey_fg = "#008751",
-   grey_fg2 = "#4c4c4c",
-   light_grey = "#545454",
-   red = "#ff004d",
+   black2 = "#181818",
+   one_bg = "#1e1e1e",
+   one_bg2 = "#252525",
+   one_bg3 = "#2c2c2c",
+   grey = "#363636",
+   grey_fg = "#3d3d3d",
+   grey_fg2 = "#454545",
+   light_grey = "#4d4d4d",
+   red = "#ff1a67",
    baby_pink = "#ff86b7",
    pink = "#ff77a8",
-   line = "#7e2553", -- for lines like vertsplit
-   green = "#008751",
-   vibrant_green = "#00e756",
-   blue = "#1d2b73",
-   nord_blue = "#101e46",
+   line = "#2c2c2c", -- for lines like vertsplit
+   green = "#00e756",
+   vibrant_green = "#10f766",
+   blue = "#29adff",
+   nord_blue = "#5c6ab2",
    yellow = "#fff024",
    sun = "#fff82c",
-   purple = "#FF2553",
-   dark_purple = "#711846",
-   teal = "#AEDCB7",
+   purple = "#a79ac0",
+   dark_purple = "#998cb2",
+   teal = "#0b925c",
    orange = "#ffa300",
    cyan = "#29adff",
-   statusline_bg = "#0c0c0c",
-   lightbg = "#191919",
-   lightbg2 = "#131313",
-   pmenu_bg = "#ffccaa",
-   folder_bg = "#3b4991",
+   statusline_bg = "#181818",
+   lightbg = "#272727",
+   pmenu_bg = "#5c6ab2",
+   folder_bg = "#29adff",
 }
 
 M.base_16 = {
-    base00 = '#030303',
-    base01 = '#3b4971',
-    base02 = '#420017',
-    base03 = '#008751',
-    base04 = '#ab5236',
-    base05 = '#8c847c',
-    base06 = '#c2c3c7',
-    base07 = '#fff1e8',
-    base08 = '#ffb20f',
-    base09 = '#ff004d',
-    base0A = '#be620a',
-    base0B = '#00e756',
-    base0C = '#29adff',
-    base0D = '#af35b9',
-    base0E = '#ff4394',
-    base0F = '#ffccaa'
+   base00 = "#101010",
+   base01 = "#171717",
+   base02 = "#1e1e1e",
+   base03 = "#252525",
+   base04 = "#2c2c2c",
+   base05 = "#d8d9dd",
+   base06 = "#d2d3d7",
+   base07 = "#cccdd1",
+   base08 = "#ffb20f",
+   base09 = "#ff004d",
+   base0A = "#be620a",
+   base0B = "#00e756",
+   base0C = "#29adff",
+   base0D = "#c54bcf",
+   base0E = "#ff4394",
+   base0F = "#ffccaa",
 }
+
+M.polish_hl = {
+   TSVariable = { fg = M.base_30.orange },
+   TSParameter = { fg = M.base_30.white },
+}
+
+M.type = "dark"
 
 M = require("base46").override_theme(M, "gatekeeper")
 
