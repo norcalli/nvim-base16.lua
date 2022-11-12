8ba2ae7e41ba587e5a25c7fd14ba81b18b3d8223
Sidhanth Rathod <siduck@tutanota.com>
2022-11-12 11:10:37 +0530

    Polish github_light theme

--- lua/base46/themes/github_light.lua
+++ lua/base46/themes/github_light.lua
@@ -1,75 +1,82 @@
+-- credits to original theme for existing https://github.com/primer/github-vscode-theme
+-- This is a modified version of it
 
 local M = {}
 
 M.base_30 = {
   white = "#24292e",
-  darker_black = "#f6f8fa",
+  darker_black = "#f3f5f7",
   black = "#ffffff", --  nvim bg
-  -- black2 = "#f6f8fa",
-  black2 = "#d4d8de",
-  one_bg = "#e1e4e8",
-  one_bg2 = "#d4d8de", -- StatusBar (filename)
-  one_bg3 = "#c5cbd3",
-  grey = "#babbbd", -- Line numbers (shouldn't be base01?)
-  grey_fg = "#6a737d", -- Why this affects comments?
-  grey_fg2 = "#99a4b2",
-  light_grey = "#8b97a7",
-  red = "#cb2431", -- StatusBar (username)
-  baby_pink = "#fae5e7",
-  pink = "#dd3c49",
-  line = "#babbbd", -- for lines like vertsplit
-  green = "#28a745", -- StatusBar (file percentage)
-  vibrant_green = "#d4f8db",
-  nord_blue = "#044289", -- Mode indicator
-  blue = "#005cc5",
-  yellow = "#b08800",
-  sun = "#dbab09",
-  purple = "#6f42c1",
+  black2 = "#edeff1",
+  one_bg = "#eaecee",
+  one_bg2 = "#e1e3e5", -- StatusBar (filename)
+  one_bg3 = "#d7d9db",
+  grey = "#c7c9cb", -- Line numbers )
+  grey_fg = "#bcbec0",
+  grey_fg2 = "#b1b3b5",
+  light_grey = "#a6a8aa",
+  red = "#DE2C2E", -- StatusBar (username)
+  baby_pink = "#ea4aaa",
+  pink = "#b93a86",
+  line = "#eaecee", -- for lines like vertsplit
+  green = "#18654B", 
+  vibrant_green = "#28a745",
+  nord_blue = "#0366d6", -- Mode indicator
+  blue = "#0D7FDD",
+  yellow = "#dbab09",
+  sun = "#f9c513",
+  purple = "#8263EB",
   dark_purple = "#5a32a3",
-  teal = "#3192aa",
-  orange = "#e36209",
+  teal = "#22839b",
+  orange = "#d15704",
   cyan = "#0598bc",
-  statusline_bg = "#e1e4e8",
-  lightbg = "#d4d8de",
-  pmenu_bg = "#032f62",
+  statusline_bg = "#edeff1",
+  lightbg = "#e1e3e5",
+  pmenu_bg = "#8263EB",
   folder_bg = "#6a737d",
 }
 
 M.base_16 = {
   base00 = "#ffffff", -- Default bg
-  base01 = "#f6f8fa", -- Lighter bg (status bar, line number, folding mks)
-  base02 = "#dbe9f9", -- Selection bg
-  base03 = "#6a737d", -- Comments, invisibles, line hl
-  base04 = "#666666", -- Dark fg (status bars)
-  base05 = "#24292e", -- Default fg (caret, delimiters, Operators)
-  base06 = "#24292e", -- Light fg (not often used)
-  base07 = "#ffffff", -- Light bg (not often used)
-  base08 = "#e36209", -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-  -- Change "Variables" with "parameters"
-  base09 = "#005cc5", -- Integers, Boolean, Constants, XML Attributes, Markup Link Url
-  base0A = "#b31d28", -- Classes, Markup Bold, Search Text Background
-  base0B = "#032f62", -- Strings, Inherited Class, Markup Code, Diff Inserted
-  -- "%p" should be recognized as 
-  base0C = "#005cc5", -- Support, regex, escape chars
-  base0D = "#6f42c1", -- Function, methods, headings
-  -- #include should not be parsed as a heading if .c
-  base0E = "#d73a49", -- Keywords
-  base0F = "#24292e", -- Deprecated, open/close embedded tags
+  base01 = "#edeff1", -- Lighter bg (status bar, line number, folding mks)
+  base02 = "#e1e3e5", -- Selection bg
+  base03 = "#d7d9db", -- Comments, invisibles, line hl
+  base04 = "#c7c9cb", -- Dark fg (status bars)
+  base05 = "#383d42", -- Default fg (caret, delimiters, Operators)
+  base06 = "#2e3338", -- Light fg (not often used)
+  base07 = "#24292e", -- Light bg (not often used)
+  base08 = "#044289", -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+  base09 = "#b93a86", -- Integers, Boolean, Constants, XML Attributes, Markup Link Url
+  base0A = "#b08800", -- Classes, Markup Bold, Search Text Background
+  base0B = "#4c2889", -- Strings, Inherited Class, Markup Code, Diff Inserted
+  base0C = "#8263EB", -- Support, regex, escape chars
+  base0D = "#005cc5", -- Function, methods, headings
+  base0E = "#DE2C2E", -- Keywords
+  base0F = "#18654B", -- Deprecated, open/close embedded tags
 }
 
 vim.opt.bg = "light"
 
 M.polish_hl = {
-  TelescopePromptPrefix = { fg = M.base_30.white },
-  TelescopeSelection = { bg = M.base_30.one_bg, fg = M.base_30.white },
-  FloatBorder = { fg = M.base_16.base05 },
-  DiffAdd = { fg = M.base_16.base05 },
-  TbLineThemeToggleBtn = { bg = M.base_30.one_bg3 },
-  WhichKeyDesc = { fg = M.base_30.white },
-  Pmenu = { bg = M.base_30.black2 },
-  St_pos_text = { fg = M.base_30.white },
-  TSVariableBuiltin = { fg = M.base_30.red },
-  TSProperty = { fg = M.base_30.teal },
+  ["@punctuation.bracket"] = {
+    fg = M.base_30.folder_bg,
+  },
+
+  ["@field.key"] = {
+    fg = M.base_30.white,
+  },
+
+  Constant = {
+    fg = M.base_16.base07,
+  },
+
+  ["@constructor"] = {
+    fg = M.base_30.vibrant_green,
+  },
+
+  Tag = {
+    fg = M.base_30.vibrant_green,
+  },
 }
 
 M = require("base46").override_theme(M, "github_light")
