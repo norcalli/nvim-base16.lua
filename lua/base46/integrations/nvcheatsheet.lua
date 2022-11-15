895523ead858d6058f187a4fe129d3d0bb38c151
siduck <siduck@tutanota.com>
2022-11-15 22:09:19 +0530

    add hl groups for nvcheatsheet

---
+++ lua/base46/integrations/nvcheatsheet.lua
@@ -0,0 +1,19 @@
+local colors = require("base46").get_theme_tb "base_30"
+
+return {
+
+  NvChHeading = {
+    fg = colors.black,
+    bg = colors.blue,
+    bold = true,
+  },
+
+  NvChSection = {
+    bg = colors.black2,
+  },
+
+  NvChAsciiHeader = {
+    fg = colors.blue,
+    bg = colors.black2,
+  },
+}
