c82a717975764116988cbb832ed909e1d88fc5fd
siduck <siduck@tutanota.com>
2022-11-15 22:18:06 +0530

    add nvdash integration

---
+++ lua/base46/integrations/nvdash.lua
@@ -0,0 +1,13 @@
+local colors = require("base46").get_theme_tb "base_30"
+
+return {
+  NvDashAscii = {
+    fg = colors.black,
+    bg = colors.blue,
+  },
+
+  NvDashButtons = {
+    fg = colors.white,
+    bg = colors.black2,
+  },
+}
