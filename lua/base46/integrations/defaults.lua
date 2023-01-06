256ae482d8fecb5424b4cc43d5a97e14dd97ca87
siduck <siduck@tutanota.com>
2023-01-06 23:23:28 +0530

    add lazy.nvim hl groups

--- lua/base46/integrations/defaults.lua
+++ lua/base46/integrations/defaults.lua
@@ -1,6 +1,8 @@
 local colors = require("base46").get_theme_tb "base_30"
 local theme = require("base46").get_theme_tb "base_16"
 
+local generate_color = require("base46.colors").change_hex_lightness
+
 return {
   MatchWord = {
     bg = colors.grey,
@@ -210,4 +212,39 @@ return {
     bg = colors.green,
     fg = colors.black,
   },
+
+  -- lazy.nvim
+  LazyH1 = {
+    bg = colors.green,
+    fg = colors.black,
+  },
+
+  LazyButton = {
+    bg = colors.one_bg,
+    fg = generate_color(colors.light_grey, vim.o.bg == "dark" and 10 or -20),
+  },
+
+  LazyH2 = {
+    bg = colors.one_bg2,
+    fg = colors.orange,
+  },
+  LazyReasonPlugin = { fg = colors.red },
+  LazyValue = { fg = colors.red },
+  LazyDir = { fg = theme.base05 },
+  LazyUrl = { fg = theme.base05 },
+  LazyCommit = { fg = colors.green },
+  LazyNoCond = { fg = colors.red },
+  LazySpecial = { fg = colors.blue },
+  LazyReasonFt = { fg = colors.purple },
+  LazyOperator = { fg = colors.white },
+  LazyReasonKeys = { fg = colors.teal },
+  LazyTaskOutput = { fg = colors.white },
+  LazyCommitIssue = { fg = colors.pink },
+  LazyReasonEvent = { fg = colors.yellow },
+  LazyReasonStart = { fg = colors.white },
+  LazyReasonRuntime = { fg = colors.nord_blue },
+  LazyReasonCmd = { fg = colors.sun },
+  LazyReasonSource = { fg = colors.cyan },
+  LazyReasonImport = { fg = colors.white },
+  LazyProgressDone = { fg = colors.green },
 }
