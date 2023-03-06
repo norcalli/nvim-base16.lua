0bd046a1a94eebb583efc1b20b688e9f6377da73
siduck <siduck@tutanota.com>
2023-03-06 11:28:46 +0530

    add some more hl groups for updater

--- lua/base46/integrations/nvchad_updater.lua
+++ lua/base46/integrations/nvchad_updater.lua
@@ -1,7 +1,13 @@
 local colors = require("base46").get_theme_tb "base_30"
 
 return {
-  nvUpdaterTitle = { fg = colors.black, bg = colors.green },
-  nvUpdaterGitPULL = { bg = colors.darker_black },
+  nvUpdaterTitle = { fg = colors.black, bg = colors.red },
+  nvUpdaterTitleDone = { fg = colors.black, bg = colors.green },
+
+  nvUpdaterGitPULL = { bg = colors.black2 },
+
   nvUpdaterProgress = { fg = colors.red, bg = colors.one_bg2 },
+  nvUpdaterProgressDONE = { fg = colors.green, bg = colors.one_bg2 },
+
+  nvUpdaterCommits = { fg = colors.green, bg = colors.black2 },
 }
