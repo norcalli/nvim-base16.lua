1f9c07911a27e74264397f0f0fa864e5e31a3da6
siduck <siduck@tutanota.com>
2023-03-06 23:14:18 +0530

    add more error related hl groups for updater

--- lua/base46/integrations/nvchad_updater.lua
+++ lua/base46/integrations/nvchad_updater.lua
@@ -1,13 +1,16 @@
 local colors = require("base46").get_theme_tb "base_30"
 
 return {
-  nvUpdaterTitle = { fg = colors.black, bg = colors.red },
+  nvUpdaterTitle = { fg = colors.black, bg = colors.orange },
   nvUpdaterTitleDone = { fg = colors.black, bg = colors.green },
+  nvUpdaterTitleFAIL = { fg = colors.black, bg = colors.red },
 
   nvUpdaterGitPULL = { bg = colors.black2 },
 
   nvUpdaterProgress = { fg = colors.red, bg = colors.one_bg2 },
   nvUpdaterProgressDONE = { fg = colors.green, bg = colors.one_bg2 },
+  nvUpdaterProgressFAIL = { fg = colors.red, bg = colors.one_bg2 },
 
   nvUpdaterCommits = { fg = colors.green, bg = colors.black2 },
+  nvUpdaterFAIL = { fg = colors.red, bg = colors.black2 },
 }
