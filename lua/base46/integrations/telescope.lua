28b362e4f0efd6def78b6efc1e355a406a34abda
siduck <siduck@tutanota.com>
2023-03-10 12:30:09 +0530

    customize telesscope hl groups for border/borderless styles

--- lua/base46/integrations/telescope.lua
+++ lua/base46/integrations/telescope.lua
@@ -1,21 +1,8 @@
 local colors = require("base46").get_theme_tb "base_30"
 
-return {
+local telescope_style = require("core.utils").load_config().ui.telescope.style
 
-  TelescopeBorder = {
-    fg = colors.darker_black,
-    bg = colors.darker_black,
-  },
-
-  TelescopePromptBorder = {
-    fg = colors.black2,
-    bg = colors.black2,
-  },
-
-  TelescopePromptNormal = {
-    fg = colors.white,
-    bg = colors.black2,
-  },
+local hlgroups = {
 
   TelescopePromptPrefix = {
     fg = colors.red,
@@ -34,22 +21,32 @@ return {
     bg = colors.red,
   },
 
-  TelescopeResultsTitle = {
-    fg = colors.darker_black,
-    bg = colors.darker_black,
-  },
-
   TelescopeSelection = { bg = colors.black2, fg = colors.white },
+  TelescopeResultsDiffAdd = { fg = colors.green },
+  TelescopeResultsDiffChange = { fg = colors.yellow },
+  TelescopeResultsDiffDelete = { fg = colors.red },
+}
 
-  TelescopeResultsDiffAdd = {
-    fg = colors.green,
+local styles = {
+  borderless = {
+    TelescopeBorder = { fg = colors.darker_black, bg = colors.darker_black },
+    TelescopePromptBorder = { fg = colors.black2, bg = colors.black2 },
+    TelescopePromptNormal = { fg = colors.white, bg = colors.black2 },
+    TelescopeResultsTitle = { fg = colors.darker_black, bg = colors.darker_black },
+    TelescopePromptPrefix = { fg = colors.red, bg = colors.black2 },
   },
 
-  TelescopeResultsDiffChange = {
-    fg = colors.yellow,
-  },
-
-  TelescopeResultsDiffDelete = {
-    fg = colors.red,
+  bordered = {
+    TelescopeBorder = { fg = colors.one_bg3 },
+    TelescopePromptBorder = { fg = colors.one_bg3 },
+    TelescopeResultsTitle = { fg = colors.black, bg = colors.green },
+    TelescopePreviewTitle = { fg = colors.black, bg = colors.blue },
+    TelescopePromptPrefix = { fg = colors.red, bg = colors.black },
+    TelescopeNormal = { bg = colors.black },
+    TelescopePromptNormal = { bg = colors.black },
   },
 }
+
+local result = vim.tbl_deep_extend("force", hlgroups, styles[telescope_style])
+
+return result
